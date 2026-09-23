d2b89089c011fb52409402709257a3d4ec5f64e8
Liqiao Liu <liqiaoliu@LiqiaodeMacBook-Air-486.local>
2026-09-23 02:50:11 +0100

    simplify the code

--- analysis_shensha_stem.py
+++ analysis_shensha_stem.py
@@ -2,27 +2,25 @@ import shensha_stem
 
 pillars = ["year", "month", "day", "hour"]
 
-def analyse_lok_san(four_pillars):
-    # 分析祿神所在柱位
-    day_stem = four_pillars["day"][0]    # 祿神的地支位置是根據日干來決定的，然後檢查四柱中哪些柱的地支與祿神地支匹配
-    lok_san = shensha_stem.Lok_San(day_stem)
+def analyse_day_stem_shensha(four_pillars, shensha_function): # shensha_function 是一個函數，根據日干返回對應的神煞地支列表
+    # 分析以日干為基準的神煞所在柱位
+    # 對多數神煞，日干是唯一的參考點，但有些神煞可能會使用年干作為參考點，這時候可以傳入不同的函數來處理
+    day_stem = four_pillars["day"][0]
+    shensha_branches = shensha_function(day_stem)
     results = []
     for pillar in pillars:
         branch = four_pillars[pillar][1]
-        if branch in lok_san:
+        if branch in shensha_branches:
             results.append(pillar)
     return results
 
+def analyse_lok_san(four_pillars):
+    # 分析祿神所在柱位
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Lok_San)
+
 def analyse_yeung_jan(four_pillars):
     # 分析羊刃所在柱位
-    day_stem = four_pillars["day"][0]     # 羊刃的地支位置是根據日干來決定的，然後檢查四柱中哪些柱的地支與羊刃地支匹配
-    yeung_jan = shensha_stem.Yeung_Jan(day_stem)
-    results = []
-    for pillar in pillars:
-        branch = four_pillars[pillar][1]
-        if branch in yeung_jan:
-            results.append(pillar)
-    return results
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Yeung_Jan)
 
 def analyse_tin_yuet(four_pillars, apply_year_stem = False):
     # 分析天乙貴人所在柱位
@@ -60,77 +58,29 @@ def analyse_man_cheung(four_pillars, apply_year_stem = False):
 
 def analyse_gam_jyu(four_pillars):
     # 分析金輿所在柱位
-    day_stem = four_pillars["day"][0]
-    gam_jyu = shensha_stem.Gam_Jyu(day_stem)
-    results = []
-    for pillar in pillars:
-        branch = four_pillars[pillar][1]
-        if branch in gam_jyu:
-            results.append(pillar)
-    return results
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Gam_Jyu)
 
 def analyse_hok_tong(four_pillars):
     # 分析學堂所在柱位
-    day_stem = four_pillars["day"][0]
-    hok_tong = shensha_stem.Hok_Tong(day_stem)
-    results = []
-    for pillar in pillars:
-        branch = four_pillars[pillar][1]
-        if branch in hok_tong:
-            results.append(pillar)
-    return results
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Hok_Tong)
+    
 
 def analyse_ci_gun(four_pillars):
     # 分析詞館所在柱位
-    day_stem = four_pillars["day"][0]
-    ci_gun = shensha_stem.Ci_Gun(day_stem)
-    results = []
-    for pillar in pillars:
-        branch = four_pillars[pillar][1]
-        if branch in ci_gun:
-            results.append(pillar)
-    return results
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Ci_Gun)
 
 def analyse_taai_gik(four_pillars):
     # 分析太極貴人所在柱位
-    day_stem = four_pillars["day"][0]
-    taai_gik = shensha_stem.Taai_Gik(day_stem)
-    results = []
-    for pillar in pillars:
-        branch = four_pillars[pillar][1]
-        if branch in taai_gik:
-            results.append(pillar)
-    return results
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Taai_Gik)
 
 def analyse_gwok_yan(four_pillars):
     # 分析國印貴人所在柱位
-    day_stem = four_pillars["day"][0]
-    gwok_yan = shensha_stem.Gwok_Yan(day_stem)
-    results = []
-    for pillar in pillars:
-        branch = four_pillars[pillar][1]
-        if branch in gwok_yan:
-            results.append(pillar)
-    return results
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Gwok_Yan)
 
 def analyse_tin_cyu(four_pillars):
     # 分析天廚貴人所在柱位
-    day_stem = four_pillars["day"][0]
-    tin_cyu = shensha_stem.Tin_Cyu(day_stem)
-    results = []
-    for pillar in pillars:
-        branch = four_pillars[pillar][1]
-        if branch in tin_cyu:
-            results.append(pillar)
-    return results
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Tin_Cyu)
 
 def analyse_fuk_sing(four_pillars):
     # 分析福星貴人所在柱位
-    day_stem = four_pillars["day"][0]
-    fuk_sing = shensha_stem.Fuk_Sing(day_stem)
-    results = []
-    for pillar in pillars:
-        branch = four_pillars[pillar][1]
-        if branch in fuk_sing:
-            results.append(pillar)
-    return results
+    return analyse_day_stem_shensha(four_pillars, shensha_stem.Fuk_Sing)
